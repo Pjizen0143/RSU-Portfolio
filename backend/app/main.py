51a530f640d6ebc8913647f0286762fefba45d49
Pjizen0143 <parabolic0143@gmail.com>
2026-04-11 17:20:59 +0700

    feat: add `/projects/` route

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 
+from app.api.api import api_router_v1
 from app.db import create_db_and_tables
 
 async def lifespan(app: FastAPI):
@@ -7,4 +8,4 @@ async def lifespan(app: FastAPI):
     yield
 
 app = FastAPI(lifespan=lifespan)
-
+app.include_router(api_router_v1)
