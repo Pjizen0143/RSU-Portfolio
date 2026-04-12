7b48b52b9e03c3b52c4b114c2a52488701f7eadd
Pjizen0143 <parabolic0143@gmail.com>
2026-04-12 22:05:10 +0700

    feat: add responsive components

--- frontend/components/ui/utils/profile-icon.tsx
+++ frontend/components/ui/utils/profile-icon.tsx
@@ -3,13 +3,14 @@ import Image from 'next/image'
 export default function ProfileCircle() {
     return (
         <div className="flex justify-center items-center">
-            <div className="pl-7 relative w-15 h-15 rounded-full overflow-hidden border-2 border-primary shadow-lg">
+            <div className="relative w-12 h-12 md:w-16 md:h-16 rounded-full overflow-hidden border-2 border-primary shadow-lg">
                 <Image
                     src="/IMG_5301.jpeg"
                     alt="Profile"
                     fill
-                    sizes='( max-width: 768px ) 100vw, 200px'
+                    sizes="(max-width: 768px) 48px, 64px"
                     className="object-cover object-top"
+                    priority
                 />
             </div>
         </div>
