37facf6d4da43f545efda2d7ee6419bd31ebf1bc
Pjizen0143 <149128910+Pjizen0143@users.noreply.github.com>
2026-04-13 20:00:20 +0700

    add mock about page (#7)

---
+++ frontend/app/(user)/about/page.tsx
@@ -0,0 +1,9 @@
+export default function AboutPage() {
+    return (
+        <div className="container mx-auto px-4 py-8">
+            <h1 className="tracking-wide text-4xl md:text-5xl font-bold text-center mx-6 mb-6">About Me</h1>
+            <p className="text-center mb-10">Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. In odio facilis quisquam dolores maxime sed accusamus quaerat ad nesciunt molestias pariatur inventore cum perferendis commodi accusantium, fuga excepturi, iure quibusdam.</p>
+        </div>
+    );
+}
