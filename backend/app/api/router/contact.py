05318e7a826c8d0c3320627b70cb50b5d797495c
Pjizen0143 <parabolic0143@gmail.com>
2026-04-13 17:43:03 +0700

    feat: contacts API endpoint

---
+++ backend/app/api/router/contact.py
@@ -0,0 +1,20 @@
+from fastapi import APIRouter, HTTPException, status
+
+from app.models import Message, ContactCreate, Contacts
+from app.db import SessionDep
+from app import crud
+
+router = APIRouter(prefix="/contacts", tags=["contacts"])
+
+
+@router.post("/", response_model=Message, status_code=status.HTTP_201_CREATED)
+async def create_contact(contact: ContactCreate, session: SessionDep):
+    contact = crud.create_contact(contact_in=contact, session=session)
+    if contact is None:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Create failed")
+    return Message(message="Contact created successfully")
+
+
+@router.get("/", response_model=list[Contacts])
+async def get_contact(session: SessionDep, skip: int = 0, limit: int = 100):
+    return crud.get_contact(skip=skip, limit=limit, session=session)
